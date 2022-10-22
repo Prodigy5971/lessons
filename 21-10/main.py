daa67a3f96c8caf6c391e094b6de21f05df107f9
SlowMotion <prodigy5971@gmail.com>
2022-10-21 22:39:53 -0300

    Mi primer commit

---
+++ 21-10/main.py
@@ -0,0 +1,10 @@
+from colorama import init, Fore, Back
+init()
+
+
+def primerClase():
+    print("Hello World")
+
+
+#print(Fore.YELLOW + 'some red text')
+#print(Back.GREEN + 'and with a green background')
